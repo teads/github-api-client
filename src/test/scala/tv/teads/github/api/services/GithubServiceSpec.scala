4d73e7ad1b2dbadb0a55d32eabb6bb7c7f74ac31
Greg Bougeard <gregory.bougeard@teads.tv>
2015-05-29 10:32:38 +0200

    Add GithubService tests

--- src/test/scala/tv/teads/github/api/services/GithubServiceSpec.scala
+++ src/test/scala/tv/teads/github/api/services/GithubServiceSpec.scala
@@ -13,7 +13,21 @@ class GithubServiceSpec extends BaseSpec {
   "Github Service" should "be able to fetch all repositories" in {
 
     whenReady(GithubService.fetchAllRepositories) { list ⇒
-//      println(list)
+      //      println(list)
+      list should not be empty
+    }
+  }
+  it should "be able to fetch service-rtb Pull Requests" in {
+
+    whenReady(GithubService.fetchPullRequests("service-rtb")) { list ⇒
+      //      println(list)
+      list should not be empty
+    }
+  }
+  it should "be able to fetch ccc-service-rtb Tags" in {
+
+    whenReady(GithubService.listTags("ccc-service-rtb")) { list ⇒
+      //      println(list)
       list should not be empty
     }
   }
