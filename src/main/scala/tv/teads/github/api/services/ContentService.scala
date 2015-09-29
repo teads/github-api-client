704d03b38e6b726660634b0dbf829b7793222140
Greg Bougeard <gregory.bougeard@teads.tv>
2015-09-29 10:57:44 +0200

    Content Service
    fetch File
    fetch Readme
    
    Content Service
    
    createFile
    editFile
    deleteFile

---
+++ src/main/scala/tv/teads/github/api/services/ContentService.scala
@@ -0,0 +1,135 @@
+package tv.teads.github.api.services
+
+import java.util.Base64
+
+import akka.actor.ActorRefFactory
+import play.api.data.mapping.Write
+import play.api.libs.json.{ JsObject, JsValue }
+import spray.http._
+import spray.httpx.RequestBuilding._
+import tv.teads.github.api.models._
+import tv.teads.github.api.services.GithubConfiguration.configuration
+import tv.teads.github.api.util._
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+object ContentService extends GithubService {
+
+  def fetchFile(org: String, repository: String, path: String)(implicit refFactory: ActorRefFactory, ec: ExecutionContext): Future[Option[String]] = {
+    val url = s"${configuration.api.url}/repos/$org/$repository/contents/$path"
+    val req: HttpRequest = Get(url)
+    baseRequest(req, Map.empty)
+      .withHeader(HttpHeaders.Accept.name, RawContentMediaType)
+      .executeRequest()
+      .map {
+        case response if response.status == StatusCodes.OK ⇒
+          Some(response.entity.asString)
+
+        case response ⇒
+          logger.error(s"fetchFile $path with url $url failed with status code ${response.status.intValue}")
+          None
+      }
+  }
+
+  def fetchFileDefaultOrg(repository: String, path: String)(implicit refFactory: ActorRefFactory, ec: ExecutionContext): Future[Option[String]] = {
+    fetchFile(configuration.organization, repository, path)
+  }
+
+  def fetchReadme(org: String, repository: String, branch: String = "master")(implicit refFactory: ActorRefFactory, ec: ExecutionContext): Future[Option[String]] = {
+    val url = s"${configuration.api.url}/repos/$org/$repository/readme"
+    val req: HttpRequest = Get(url)
+    baseRequest(req, Map("ref" → branch))
+      .withHeader(HttpHeaders.Accept.name, RawContentMediaType)
+      .executeRequest()
+      .map {
+        case response if response.status == StatusCodes.OK ⇒
+          Some(response.entity.asString)
+
+        case response ⇒
+          logger.error(s"fetchReadme with url $url failed with status code ${response.status.intValue}")
+          None
+      }
+  }
+
+  def fetchReadmeDefaultOrg(repository: String, branch: String = "master")(implicit refFactory: ActorRefFactory, ec: ExecutionContext): Future[Option[String]] = {
+    fetchReadme(configuration.organization, repository, branch)
+  }
+
+  case class FileCreateParam(
+    path:      String,
+    message:   String,
+    content:   String,
+    branch:    Option[String] = None,
+    committer: Option[Author],
+    author:    Option[Author]
+  )
+
+  implicit lazy val fileCreateParamJsonWrite: Write[FileCreateParam, JsValue] = {
+    import play.api.data.mapping.json.Writes._
+    Write.gen[FileCreateParam, JsObject]
+  }
+
+  def createFile(org: String, repository: String, file: FileCreateParam)(implicit refFactory: ActorRefFactory, ec: ExecutionContext): Future[Boolean] = {
+    val url = s"${configuration.api.url}/repos/$org/$repository/content/${file.path}"
+    val req: HttpRequest = Put(url, file.copy(content = Base64.getEncoder.encodeToString(file.content.getBytes)))
+    baseRequest(req, Map.empty)
+      .withHeader(HttpHeaders.Accept.name, RawContentMediaType)
+      .executeRequest()
+      .map {
+        case response if response.status == StatusCodes.OK ⇒
+          true
+
+        case response ⇒
+          logger.error(s"cannot create file  with url $url failed with status code ${response.status.intValue}")
+          false
+      }
+  }
+
+  case class FileEditParam(
+    path:      String,
+    message:   String,
+    content:   String,
+    sha:       String,
+    branch:    Option[String] = None,
+    committer: Option[Author],
+    author:    Option[Author]
+  )
+
+  implicit lazy val fileEditParamJsonWrite: Write[FileEditParam, JsValue] = {
+    import play.api.data.mapping.json.Writes._
+    Write.gen[FileEditParam, JsObject]
+  }
+
+  def editFile(org: String, repository: String, file: FileEditParam)(implicit refFactory: ActorRefFactory, ec: ExecutionContext): Future[Boolean] = {
+    val url = s"${configuration.api.url}/repos/$org/$repository/content/${file.path}"
+    val req: HttpRequest = Put(url, file.copy(content = Base64.getEncoder.encodeToString(file.content.getBytes)))
+    baseRequest(req, Map.empty)
+      .withHeader(HttpHeaders.Accept.name, RawContentMediaType)
+      .executeRequest()
+      .map {
+        case response if response.status == StatusCodes.OK ⇒
+          true
+
+        case response ⇒
+          logger.error(s"Cannot edit file ${file.path} with url $url failed with status code ${response.status.intValue}")
+          false
+      }
+  }
+
+  def deleteFile(org: String, repository: String, file: FileEditParam)(implicit refFactory: ActorRefFactory, ec: ExecutionContext): Future[Boolean] = {
+    val url = s"${configuration.api.url}/repos/$org/$repository/content/${file.path}"
+    val req: HttpRequest = Delete(url)
+    baseRequest(req, Map.empty)
+      .withHeader(HttpHeaders.Accept.name, RawContentMediaType)
+      .executeRequest()
+      .map {
+        case response if response.status == StatusCodes.OK ⇒
+          true
+
+        case response ⇒
+          logger.error(s"Cannot delete file ${file.path} with url $url failed with status code ${response.status.intValue}")
+          false
+      }
+  }
+
+}
--- src/main/scala/tv/teads/github/api/services/RepositoryService.scala
+++ src/main/scala/tv/teads/github/api/services/RepositoryService.scala
@@ -12,22 +12,6 @@ import scala.concurrent.{ ExecutionContext, Future }
 
 object RepositoryService extends GithubService {
 
-  def fetchFile(repository: String, path: String)(implicit refFactory: ActorRefFactory, ec: ExecutionContext): Future[Option[String]] = {
-    val url = s"${configuration.api.url}/repos/${configuration.organization}/$repository/contents/$path"
-    val req: HttpRequest = Get(url)
-    baseRequest(req, Map.empty)
-      .withHeader(HttpHeaders.Accept.name, RawContentMediaType)
-      .executeRequest()
-      .map {
-        case response if response.status == StatusCodes.OK ⇒
-          Some(response.entity.asString)
-
-        case response ⇒
-          logger.error(s"fetchFile with url $url failed with status code ${response.status.intValue}")
-          None
-      }
-  }
-
   def listTags(repository: String)(implicit refFactory: ActorRefFactory, ec: ExecutionContext): Future[List[Tag]] = {
     import play.api.data.mapping.json.Rules._
     val route = s"repos/${configuration.organization}/$repository/tags"
