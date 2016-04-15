7de3175ae146caf3d5433c065c7142060d04f73b
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2016-04-15 20:48:55 +0200

    wip

--- src/main/scala/tv/teads/github/api/GithubApiClient.scala
+++ src/main/scala/tv/teads/github/api/GithubApiClient.scala
@@ -3,7 +3,6 @@ package tv.teads.github.api
 import java.io.File
 
 import tv.teads.github.api.http.{OkHttpClientWrapper, Authenticator}
-import tv.teads.github.api.services._
 
 private[api] case class GithubApiClientConfig(
     owner:         String,
@@ -17,22 +16,5 @@ private[api] case class GithubApiClientConfig(
 }
 
 case class GithubApiClient private[api] (config: GithubApiClientConfig) {
-  val contents = new ContentService(config)
-  val hooks = new HookService(config)
-  val issues = new IssueService(config)
-  val labels = new LabelService(config)
-  val members = new MemberService(config)
-  val organizations = new OrganizationService(config)
-  val pullRequests = new PullRequestService(config)
-  val rateLimit = new RateLimitService(config)
-  val repositories = new RepositoryService(config)
-  val statuses = new StatusService(config)
-  val teams = new TeamService(config)
-  val users = new UserService(config)
-  val releases = new ReleaseService(config)
-  val commits = new CommitService(config)
-  val deployments = new DeploymentService(config)
-  val references = new ReferenceService(config)
-
   def clearCache(): Unit = config.client.clearCache()
 }
