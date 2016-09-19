66a9c9a56527d6d4852e7193e77f860bbe5363a8
Greg Bougeard <gregory.bougeard@teads.tv>
2016-09-19 16:14:06 +0200

    Handle new action types for webhooks

--- src/main/scala/tv/teads/github/api/model/webhook/PullRequestAction.scala
+++ src/main/scala/tv/teads/github/api/model/webhook/PullRequestAction.scala
@@ -4,7 +4,7 @@ import tv.teads.github.api.util.Enumerated
 
 sealed trait PullRequestAction
 object PullRequestAction extends Enumerated[PullRequestAction] {
-  val values = List(assigned, unassigned, labeled, unlabeled, opened, closed, reopened, synchronize)
+  val values = List(assigned, unassigned, labeled, unlabeled, opened, closed, reopened, synchronize, edited)
 
   case object assigned extends PullRequestAction
   case object unassigned extends PullRequestAction
@@ -14,4 +14,5 @@ object PullRequestAction extends Enumerated[PullRequestAction] {
   case object closed extends PullRequestAction
   case object reopened extends PullRequestAction
   case object synchronize extends PullRequestAction
+  case object edited extends PullRequestAction
 }
--- src/main/scala/tv/teads/github/api/model/webhook/RepositoryAction.scala
+++ src/main/scala/tv/teads/github/api/model/webhook/RepositoryAction.scala
@@ -4,7 +4,10 @@ import tv.teads.github.api.util.Enumerated
 
 sealed trait RepositoryAction
 object RepositoryAction extends Enumerated[RepositoryAction] {
-  val values = List(created)
+  val values = List(created, deleted, publicized, privatized)
 
   case object created extends RepositoryAction
+  case object deleted extends RepositoryAction
+  case object publicized extends RepositoryAction
+  case object privatized extends RepositoryAction
 }
